bee472b4641df402fc3cca7d8c58a0e73441108a
Derek Tran <dtran110@my.bcit.ca>
2023-03-09 14:56:24 -0800

    Add test file and doctests to reverse_concat

--- exceptions_quiz.py
+++ exceptions_quiz.py
@@ -11,20 +11,24 @@ def reverse_concat(first_word, second_word):
     :postcondition: concatenates then reverses first_word and second_word
     :return: first_word and second_word concatenated then reversed as a string
     :raises TypeError: if first_word is not a string
-    :raises TypeError: if second is not a string
+    :raises TypeError: if second_word is not a string
     :raises ValueError: if first_word is an empty string
     :raises ValueError: if second_word is an empty string
-
+    >>> reverse_concat("one", "two")
+    'owteno'
+    >>> reverse_concat("123456", "789")
+    '987654321'
+    >>> reverse_concat("desreveR"," sI sihT")
+    'This Is Reversed'
     """
-    if first_word is not str or second_word is not str:
-        raise TypeError("")
-
+    if type(first_word) is not str or type(second_word) is not str:
+        raise TypeError("Please input strings")
     elif first_word == "" or second_word == "":
-        raise ValueError("")
-
+        raise ValueError("Your inputs cannot be empty")
     else:
-        result = first_word + second_word
-        return result.reverse()
+        first_result = first_word + second_word
+        final_result = (first_result[::-1])
+        return final_result
 
 
 def main():
@@ -34,11 +38,11 @@ def main():
     first_word = input("please enter the first word")
     second_word = input("please enter the second word")
     try:
-        reverse_concat(first_word, second_word)
+        result = reverse_concat(first_word, second_word)
     except ValueError as error_message:
         print(error_message)
     else:
-        print("")
+        print(result)
 
 
 if __name__ == "__main__":
---
+++ reverse_concat.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestReverseConcat(TestCase):
+    def test_reverse_concat(self):
+        self.fail()
