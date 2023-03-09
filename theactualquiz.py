2be672a667f36537810b10c70bc99adc1b13a79d
Derek Tran <dtran110@my.bcit.ca>
2023-03-09 14:43:32 -0800

    Define reverse_concat function with docstring

---
+++ theactualquiz.py
@@ -0,0 +1,27 @@
+def reverse_concat(first_word,second_word):
+    """
+    Concatenate and reverse first_word and second_word.
+
+    :param first_word: a string
+    :param second_word: a string
+    :precondition: first_word must not be empty
+    :precondition: first_word must be a string
+    :precondition: second_word must not be empty
+    :precondition: second_word must be a string
+    :postcondition: concatenates then reverses first_word and second_word
+    :return: first_word and second_word concatenated then reversed as a string
+    :raises TypeError: if first_word is not a string
+    :raises TypeError: if second is not a string
+    :raises ValueError: if first_word is an empty string
+    :raises ValueError: if second_word is an empty string
+
+    """
+    if first_word is not str or second_word is not str:
+        raise TypeError("")
+
+    elif first_word == "" or second_word == "":
+        raise ValueError("")
+
+    else:
+        result = first_word + second_word
+        return result.reverse()
