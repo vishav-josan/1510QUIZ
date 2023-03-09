4b7f56a93bc4bdfb985f4697cdb6df9dfbdb32b3
Derek Tran <dtran110@my.bcit.ca>
2023-03-09 14:58:23 -0800

    Add first unit test

--- reverse_concat.py
+++ reverse_concat.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
+from exceptions_quiz import reverse_concat
 
 
 class TestReverseConcat(TestCase):
-    def test_reverse_concat(self):
-        self.fail()
+    def test_reverse_concat_standard(self):
+        self.assertEqual("This Is Reversed", reverse_concat("desreveR", " sI sihT"))
